09b701c7e51d6564cb2d57f058496546e98d9383
vips95 <arora.vips@gmail.com>
2015-10-05 19:32:00 -0400

    added game controller script

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public int cloudCount;
+	public GameObject cloud;
+	
+	// Use this for initialization
+	void Start () {
+		this._GenerateClouds ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	// generate Clouds
+	private void _GenerateClouds() {
+		for (int count=0; count < this.cloudCount; count++) {
+			Instantiate(cloud);
+		}
+	}
+}
